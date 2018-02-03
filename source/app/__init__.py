7e79aa9420c5c1f31235f757d33f4e59b6e6d8c6
joerisolie <Dgmt88rt>
2018-02-03 20:07:48 +0100

    Added support for username/password configuration

--- source/app/__init__.py
+++ source/app/__init__.py
@@ -5,7 +5,7 @@ import pymongo
 app = Flask(__name__)
 app.config.from_object('config')
 
-app.mongo_client = pymongo.MongoClient(host=os.getenv('MONGO_HOST', 'localhost'), port=int(os.getenv('MONGO_PORT', '27017')))
+app.mongo_client = pymongo.MongoClient(host=os.getenv('MONGO_HOST', 'localhost'), port=int(os.getenv('MONGO_PORT', '27017')), username=os.getenv('MONGO_USERNAME', None), password=os.getenv('MONGO_PASSWORD', None))
 
 from app import views
 from app.views import check_init_kvs
